434590016dc0a6f6e03764b588a2046342ffa746
sudozid <67092879+sudozid@users.noreply.github.com>
2021-09-28 16:46:43 +0530

    Update tlscheck.py

--- tlscheck.py
+++ tlscheck.py
@@ -1,6 +1,8 @@
 import socket, ssl
 import csv
 from concurrent.futures import ThreadPoolExecutor
+import numpy as np
+
 
 context = ssl.SSLContext()
 context.verify_mode = ssl.CERT_REQUIRED
@@ -15,22 +17,35 @@ with open('majestic_million.csv', 'r',encoding = 'cp850') as rf:
     sites.extend([i[2]]) #domain is in 3rd column of csv file
 print("File open done")
 sites.pop(0) #remove column header
+sites=np.array(sites)
+print("List to array complete")
+sites=np.array_split(sites,2) #most computers should handle a million domains fine but if you want to do more split the array into more chunks
+print("List split complete")
+sites=list(sites)
 
 def check_ssl(x):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ssl_sock = context.wrap_socket(s, server_hostname=x)
+    ssl_sock.settimeout(3.0)
     try:
         ssl_sock.connect(('1.1.1.1', 443))
         ssl_sock.close()
     except ConnectionResetError:
-        w = open("blockedsites4.txt", "a")
+        w = open("blockedsites5.txt", "a")
         w.write("\n" + x)
         print( x + " is blocked")
         w.close()
         ssl_sock.close()
-    except:
+    except ssl.SSLCertVerificationError:
         ssl_sock.close()
         pass
+    except socket.timeout:
+        ssl_sock.close()
+        pass
+    except Exception as e:
+        ssl_sock.close()
+        print(e)
 
-with ThreadPoolExecutor(max_workers=40) as pool:
- response_list = list(pool.map(check_ssl, sites))
+for z in sites:
+    with ThreadPoolExecutor(max_workers=32) as pool:
+        response_list = list(pool.map(check_ssl, z))
